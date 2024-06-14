c11a9c3722449a0d012b1275b09cdf099851f77e
Joel Barton <joelbarton406@gmail.com>
2024-06-14 10:52:36 -0600

    73.

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import Card from '../../shared/components/UIElements/Card';
-import Button from '../../shared/components/FormElements/Button';
-import Modal from '../../shared/components/UIElements/Modal';
-import Map from '../../shared/components/UIElements/Map';
-import './PlaceItem.css';
+import Card from '../../shared/components/UIElements/Card'
+import Button from '../../shared/components/FormElements/Button'
+import Modal from '../../shared/components/UIElements/Modal'
+import Map from '../../shared/components/UIElements/Map'
+import './PlaceItem.css'
 
-const PlaceItem = props => {
-  const [showMap, setShowMap] = useState(false);
+const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false)
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
 
-  const openMapHandler = () => setShowMap(true);
+  const openMapHandler = () => setShowMap(true)
+  const closeMapHandler = () => setShowMap(false)
 
-  const closeMapHandler = () => setShowMap(false);
-
+  const showDeleteHandler = () => setShowConfirmModal(true)
+  const cancelDeleteHandler = () => setShowConfirmModal(false)
+  const confirmDeleteHandler = () => {
+    console.log('DELETED ON SERVER')
+    setShowConfirmModal(false)
+  }
   return (
     <React.Fragment>
       <Modal
@@ -27,6 +33,27 @@ const PlaceItem = props => {
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              DELETE
+            </Button>
+          </>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that it
+          cannot be undone thereafter
+        </p>
+      </Modal>
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -38,14 +65,18 @@ const PlaceItem = props => {
             <p>{props.description}</p>
           </div>
           <div className="place-item__actions">
-            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={showDeleteHandler}>
+              DELETE
+            </Button>
           </div>
         </Card>
       </li>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default PlaceItem;
+export default PlaceItem
