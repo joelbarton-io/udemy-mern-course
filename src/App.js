cf1f4b08b574b96fa96036cb1a574ee387fe185e
Joel Barton <joelbarton406@gmail.com>
2024-06-25 11:10:14 -0600

    pre-adding build to backend public folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch,
 } from 'react-router-dom'
-
 import Users from './user/pages/Users'
-import NewPlace from './places/pages/NewPlace'
-import UpdatePlace from './places/pages/UpdatePlace'
-import UserPlaces from './places/pages/UserPlaces'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
-import Auth from './user/pages/Auth'
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner'
 import { AuthContext } from './shared/context/auth-context'
 import { useAuth } from './shared/hooks/auth'
+
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'))
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'))
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'))
+const Auth = React.lazy(() => import('./user/pages/Auth'))
 
 const App = () => {
   const { token, loggedinUserid, login, logout } = useAuth()
@@ -63,7 +64,17 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
       </Router>
     </AuthContext.Provider>
   )
